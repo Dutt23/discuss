c1595321ba2fd4447465d82774fd9db6466cf476
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2024-11-28 20:25:12 +0530

    Get data

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,20 @@
-export default function TopicShowPage() {
-  return <div>Topic Show</div>
+interface TopicShowPageProps {
+  params: Promise<{ slug: string }>
+}
+
+export default async function TopicShowPage({ params }: Readonly<TopicShowPageProps>) {
+  const slug = (await params).slug
+  return (
+    <div className="grid grid-cold-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-2xl font-bold mb-2">
+          {slug}
+        </h1>
+      </div>
+
+      <div>
+        
+      </div>
+    </div>
+  )
 }
