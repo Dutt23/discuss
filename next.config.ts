32b8790fa6bb7aa3741c9e3a3a5c63df34eef5d0
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2024-12-02 13:25:26 +0530

    show details

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/u/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
